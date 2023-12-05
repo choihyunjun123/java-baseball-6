51b9bf3673e689d0821e2120588b38ff7abb2710
HYUNJUN\sajun <sajun28@naver.com>
2023-12-05 15:08:59 +0900

    Feat: "게임 시작"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,10 @@
 package baseball;
 
+import baseball.view.OutputView;
+
 public class Application {
-
+    private static final OutputView outputView = new OutputView();
     public static void main(String[] args) {
-
+        outputView.start();
     }
 }
