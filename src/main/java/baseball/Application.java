6e5c608c9901e4786a062feb12eff22666468389
HYUNJUN\sajun <sajun28@naver.com>
2023-12-05 15:57:56 +0900

    Feat: "랜덤 숫자 생성 추가"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,29 @@
 package baseball;
 
+import baseball.model.RandomNumber;
 import baseball.model.Verify;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
+import java.util.List;
+
 public class Application {
+
+    private static List<Integer> RandomNum;
+    private static String InputNum;
     private static final OutputView outputView = new OutputView();
     private static final InputView inputView = new InputView();
     private static final Verify verify = new Verify();
+    private static final RandomNumber randomNumber = new RandomNumber();
+
+    Application() {
+        RandomNum = randomNumber.generateNum();
+    }
 
     public static void inputNum() {
+        InputNum = inputView.inputNum();
         try {
-            verify.inputNum(inputView.inputNum());
+            verify.inputNum(InputNum);
         } catch (IllegalArgumentException e) {
             outputView.error(e.getMessage());
             inputNum();
